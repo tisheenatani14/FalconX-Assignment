aece5135bb607b91884c9431e7b7ab6f01ff6473
tisheenatani14 <103897041+tisheenatani14@users.noreply.github.com>
2022-04-18 00:20:10 +0530

    Update FalconX_Q3.py

--- FalconX_Q3.py
+++ FalconX_Q3.py
@@ -3,6 +3,7 @@ import pandas as pd
 data = []
 df = pd.read_csv('file1.csv')
 
+#calculating the final needed values by reading the previously created file in Q2
 for i in range(len(df)):
     gross_pnl_basis_points = (df.iloc[i, 2]/df.iloc[i, 1])*10000
     gross_pnl_usd = df.iloc[i, 2]/df.iloc[i, 1]
@@ -13,6 +14,7 @@ for i in range(len(df)):
     data.append([gross_pnl_basis_points, gross_pnl_usd, maximum_drawdown, average_pnl, number_of_trades])
 
 df_0 = pd.DataFrame(data, columns=['Gross PnL in Basis Points', 'Gross PnL in USD', 'Maximum Drawdown', 'Average PnL', 'Number of Trades'])
+#creating the final result csv file
 df_0.to_csv('Final.csv')
 
 
